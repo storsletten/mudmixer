2e43d0f2075b81e2d5d5f95c823ccf49a3b010a7
tms88 <tms88@users.noreply.github.com>
2021-04-01 05:28:03 +0200

    Add command MX SK

---
+++ middleware/.client/mxCommands/session-kill.js
@@ -0,0 +1,29 @@
+module.exports = (main, middleware) => {
+ const exports = main.exports;
+
+ return {
+  syntax: ``,
+  commands: ['mxsk'],
+  aliases: ['sk', 'skill', 'kill-session'],
+  help: [
+   `This command disconnects all server connections associated with the current session.`,
+  ],
+  action: function({ device, argstr }) {
+   const session = device.session;
+   if (session.servers.size === 0) return device.tell(`This session has no servers to kill.`);
+   const killedServers = [];
+   session.servers.forEach(server => {
+    if (server.serverOptions.disabled !== true) {
+     server.serverOptions.disabled = true;
+     server.disconnect();
+     killedServers.push(server);
+    }
+   });
+   if (killedServers.length === 0) device.tell(`${session.servers.size === 1 ? `The server connection is` : `All server connections are`} already disconnected.`);
+   else {
+    if (killedServers.length > 1) device.tell(`Total: ${killedServers.length}`);
+    session.save();
+   }
+  },
+ };
+};
